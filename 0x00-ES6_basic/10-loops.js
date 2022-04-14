64852e5458ea229e9df26d4d79c741486f5dbc16
Sebatian <sebastian@nufi.mx>
2022-04-14 13:23:37 -0500

    Fix 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,4 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const task = 3;
-  console.log(task);
   for (const item of array) {
     array[array.indexOf(item)] = appendString + item;
   }
