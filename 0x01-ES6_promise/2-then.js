e66187a23fcde46fe6836beeca1eabf99f46f8c3
Juan Sebastian Caicedo <juan.caicedo@encora.com>
2022-07-10 22:11:20 -0500

    Fix 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise
-      .then(() => resolve({ status: 200, body: 'Success' }))
+      .then(() => resolve({ status: 200, body: 'success' }))
       .catch(() => reject(new Error()));
     console.log('Got a response from the API');
   });
